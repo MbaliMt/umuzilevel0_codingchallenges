e2f1203a1c5d40461c252d2bd899586f71bece38
MbaliMt <mz.mthimkhulu@gmail.com>
2021-04-20 06:15:24 +0200

    modified Task 10

--- Task10.js
+++ Task10.js
@@ -3,10 +3,10 @@ function myString(word1, word2){
     for (let x = 0; x < word1.length; x++){  
         for (let n = 0; n < word2.length; n++){
             if (word1[x] == word2[n]){
-                console.log(word2[n]);
+                process.stdout.write(word2[n]);
             }
         }    
-    }              
+    }console.log();          
 }
 myString("house", "computers");
 
