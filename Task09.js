ca169c7cee40ba98f05a509174d321af63ac2245
MbaliMt <mz.mthimkhulu@gmail.com>
2021-04-20 06:12:59 +0200

    Modified task 09

--- Task09.js
+++ Task09.js
@@ -3,11 +3,13 @@ function myString(word){
     for (let x = 0; x < word.length; x++){  
         for (let n = 0; n < vowels.length; n++){
             if (word[x] == vowels[n]){
-                console.log(vowels[n]);
+                process.stdout.write(vowels[n])
             }
         }    
-    }              
+    }console.log();              
 }
+myString("Umuzi".toLowerCase())
+
 
 
 
