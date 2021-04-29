0f877209d0e8718f5bde758f548427a76046244a
MbaliMt <mz.mthimkhulu@gmail.com>
2021-04-29 10:50:59 +0200

    put spaces in between my vowels

--- Task10.js
+++ Task10.js
@@ -5,13 +5,12 @@ function myString(word1, word2){
     for (let x = 0; x < word1.length; x++){  
         for (let n = 0; n < word2.length; n++){
             if (word1[x] == word2[n]){
-                process.stdout.write(word2[n]);
-                newWord.push(word2[n]);               
+                newWord.push(" "+ word2[n]);               
             }
         }    
-    }console.log();     
+    }   
     newWord = newWord.toString().split(",")
-    console.log(newWord.toString());
+    console.log("Common letters:"+newWord.toString());
     }
 myString("house", "computers");
 
